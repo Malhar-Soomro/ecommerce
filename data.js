6108cb4feb24d43b9f23964e839f680299389d8a
Malhar-Soomro <malharsoomro2@gmail.com>
2023-12-16 17:02:07 +0500

    assets folder moved in public

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export const sliderData = [
         id: 1,
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        image: "./src/assets/slider-image-1.jpg",
+        image: "/assets/slider-image-1.jpg",
         bg: "#f5fafd",
     },
 
@@ -11,7 +11,7 @@ export const sliderData = [
         id: 2,
         title: "LOUNGEWEAR LOVE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 40% OFF FOR NEW ARRIVALS.",
-        image: "/src/assets/slider-image-2.jpg",
+        image: "/assets/slider-image-2.jpg",
         bg: "#fcf1ed",
     },
 
@@ -19,7 +19,7 @@ export const sliderData = [
         id: 3,
         title: "AUTUMN COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
-        image: "/src/assets/slider-image-3.jpg",
+        image: "/assets/slider-image-3.jpg",
         bg: "#fbf0f4",
     }
 ]
@@ -28,53 +28,53 @@ export const categoryData = [
     {
         id: 1,
         title: "SHIRT STYLE!",
-        image: "/src/assets/category-image-1.jpg",
+        image: "/assets/category-image-1.jpg",
     },
 
     {
         id: 2,
         title: "LOUNGEWEAR LOVE",
-        image: "/src/assets/category-image-2.jpg",
+        image: "/assets/category-image-2.jpg",
     },
 
     {
         id: 3,
         title: "LIGHT JACKETS",
-        image: "/src/assets/category-image-3.jpg",
+        image: "/assets/category-image-3.jpg",
     }
 ]
 
 export const productsData = [
     {
         id: 1,
-        img: "/src/assets/products-image-1.png"
+        img: "/assets/products-image-1.png"
     },
     {
         id: 2,
-        img: "/src/assets/products-image-2.png"
+        img: "/assets/products-image-2.png"
     },
     {
         id: 3,
-        img: "/src/assets/products-image-3.png",
+        img: "/assets/products-image-3.png",
     },
     {
         id: 4,
-        img: "/src/assets/products-image-4.png"
+        img: "/assets/products-image-4.png"
     },
     {
         id: 5,
-        img: "/src/assets/products-image-5.png",
+        img: "/assets/products-image-5.png",
     },
     {
         id: 6,
-        img: "/src/assets/products-image-6.png",
+        img: "/assets/products-image-6.png",
     },
     {
         id: 7,
-        img: "/src/assets/products-image-7.png",
+        img: "/assets/products-image-7.png",
     },
     // {
     //     id: 8,
-    //     img: "/src/assets/products-image-8.png",
+    //     img: "/assets/products-image-8.png",
     // },
 ]
