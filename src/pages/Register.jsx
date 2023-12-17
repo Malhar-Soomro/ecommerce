541ad977f6e8363690388675470f41b4fc353cd6
Malhar-Soomro <malharsoomro2@gmail.com>
2023-12-17 08:27:13 +0500

    add responsive styles in register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,17 +2,17 @@
 const Register = () => {
   return (
     <div className="flex justify-center items-center h-[100vh] bg-pink-200">
-        <div className="bg-white p-6 flex flex-col justify-center items-center gap-2 w-[45%]">
+        <div className="bg-white p-6 flex flex-col justify-center items-center gap- sm:w-[45%] w-[90%]">
             <h2 className="text-2xl font-light mr-auto">CREATE AN ACCOUNT</h2>
-            <div className="flex gap-2 justify-center w-full text-sm mt-4">
+            <div className="flex gap-4 sm:gap-2 justify-center w-full text-sm mt-4 flex-col sm:flex-row">
                 <input className="border border-black p-2 w-full" type="text" placeholder="name" />
                 <input className="border border-black p-2 w-full" type="text" placeholder="last name" />
             </div>
-            <div className="flex gap-2 w-full text-sm mt-4">
+            <div className="flex gap-4 sm:gap-2 w-full text-sm mt-4 flex-col sm:flex-row">
                 <input className="border border-black p-2 w-full" type="text" placeholder="username" />
                 <input className="border border-black p-2 w-full" type="text" placeholder="email" />
             </div>
-            <div className="flex gap-2 w-full text-sm mt-4">
+            <div className="flex gap-4 sm:gap-2 w-full text-sm mt-4 flex-col sm:flex-row">
                 <input className="border border-black p-2 w-full" type="text" placeholder="password" />
                 <input className="border border-black p-2 w-full" type="text" placeholder="confirm password" />
             </div>
