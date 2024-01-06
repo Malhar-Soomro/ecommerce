b48fc75ae7fa5939c485b47d3ac69af41b641b52
Malhar-Soomro <malharsoomro2@gmail.com>
2024-01-06 20:35:38 +0500

    implemented login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { login } from "../redux/apiCalls";
+import { useDispatch, useSelector } from "react-redux";
 
 const Login = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const {isFetching, error} = useSelector(state => state.user);
+
+  const dispatch = useDispatch();
+
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    login(dispatch, {username, password});
+  }
   return (
     <div className="flex justify-center items-center h-[100vh] bg-gray-200">
       <div className="flex flex-col bg-white sm:w-[28%] md:w-[40%] lg:w-[28%] w-[90%] p-4 gap-2">
         <h2 className="text-2xl font-light mr-auto">SIGN IN</h2>
-        <input className="border border-black p-2 w-full text-sm mt-2" type="text" placeholder="username" />
-        <input className="border border-black p-2 w-full text-sm mt-2" type="text" placeholder="password" />
-        <button className="bg-[#008080] mr-auto w-[40%] py-3 text-white text-sm">LOGIN</button>
-        <a className="underline text-xs mt-2" href="">DO NOT REMEMBER THE PASSWORD?</a>
-        <a className="underline text-xs" href="">CREATE A NEW ACCOUNT</a>
+        <input className="border border-black p-2 w-full text-sm mt-2" type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)} />
+        <input className="border border-black p-2 w-full text-sm mt-2" type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleOnClick} className="bg-[#008080] mr-auto w-[40%] py-3 text-white text-sm disabled:text-[#008080] disabled:hover:cursor-not-allowed" disabled={isFetching}>LOGIN</button>
+        {error && <p className="text-red-700">Something went wrong</p>}
+        <Link className="underline text-xs mt-2" href="#">DO NOT REMEMBER THE PASSWORD?</Link>
+        <Link className="underline text-xs" href="#">CREATE A NEW ACCOUNT</Link>
       </div>
     </div>
-  )
-
+  );
 }
 
 export default Login
---
+++ src/redux/apiCalls.js
@@ -0,0 +1,13 @@
+import { loginFail, loginStart, loginSuccess } from "./userRedux";
+import { publicRequest } from "../requestMethod";
+
+export const login = async (dispatch, user) => {
+    dispatch(loginStart());
+    try {
+        const res = await publicRequest.post("/auth/login", user);
+        dispatch(loginSuccess(res.data))
+
+    } catch (error) {
+        dispatch(loginFail());
+    }
+}
