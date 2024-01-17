ed258fc5a2baa6f0540e27c4a4d2e3e695dbd09c
Malhar-Soomro <malharsoomro2@gmail.com>
2024-01-17 10:15:48 +0500

    changed the base url

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://ecommerce-backend-yxgm.onrender.com/api";
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://ecommerce-backend-yxgm.onrender.com/api";
+// const BASE_URL = "http://localhost:5000/api/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NzkzNjJlNDlmODVkNmQxMjU2MGE5ZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwNDk3MDQ1OCwiZXhwIjoxNzA1MTQzMjU4fQ.ZNYAIzrzRclDAcQ5YMT6-TCNIVhfKPd5j4fDJvz27Nc";
 
 export const publicRequest = axios.create({
